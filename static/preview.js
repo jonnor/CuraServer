dd838aa2bacd25e4f9eeab84debb94aad70c43bb
Jon Nordby <jononor@gmail.com>
2014-08-17 23:18:31 +0200

    Add OrbitControls to preview
    Still needs some build step to copy the library files

--- static/preview.js
+++ static/preview.js
@@ -1,17 +1,28 @@
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-var renderer = new THREE.WebGLRenderer(); renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild(renderer.domElement);
+var controls, renderer, scene, camera;
 
-var geometry = new THREE.BoxGeometry(1,1,1);
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material ); scene.add( cube );
-camera.position.z = 5;
+function animate() {
+   requestAnimationFrame(animate);
+   controls.update();
+}
 
 function render() {
-    requestAnimationFrame(render); renderer.render(scene, camera);
-    cube.rotation.x += 0.02;
-    cube.rotation.y += 0.02;
+    renderer.render(scene, camera);
 }
-render();
+
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    renderer = new THREE.WebGLRenderer(); renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild(renderer.domElement);
+    controls = new THREE.OrbitControls(camera);
+    controls.addEventListener('change', render)
+
+    var geometry = new THREE.BoxGeometry(1,1,1);
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material ); scene.add( cube );
+    camera.position.z = 5;
+
+    render();
+}
+init();
