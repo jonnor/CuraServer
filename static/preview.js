43879f8b9e5a2874d8d9a05771695113d25baa6d
Jon Nordby <jononor@gmail.com>
2014-08-18 00:53:13 +0200

    Load dummy model data into 3d preview

--- static/preview.js
+++ static/preview.js
@@ -1,9 +1,34 @@
 
-var controls, renderer, scene, camera;
+function meshFromPolygon(polygon, xoffset, yoffset) {
+    var geometry = new THREE.Geometry();
+
+    // Generate the vertices of the n-gon.
+    for (var i = 0; i < polygon.length; i++) {
+        var p = polygon[i];
+        console.log(p.length);
+        geometry.vertices.push(new THREE.Vector3(p[0]-xoffset, p[1]-yoffset, p[2]));
+    }
+    // Generate the faces of the n-gon.
+    for (i = 0; i < polygon.length-2; i++) {
+        geometry.faces.push(new THREE.Face3(0, i+1, i+2));
+    }
+
+    geometry.computeBoundingSphere();
+
+    var material = new THREE.MeshBasicMaterial({color:"#ff0000"});
+    var mesh = new THREE.Mesh(geometry, material);
+    return mesh;
+}
+
+var controls, renderer, scene, camera, obj;
 
 function animate() {
-   requestAnimationFrame(animate);
-   controls.update();
+    requestAnimationFrame(animate);
+    if (obj) {
+        obj.rotation.y += 0.01;
+    }
+    controls.update();
+    render();
 }
 
 function render() {
@@ -13,16 +38,37 @@ function render() {
 function init() {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    renderer = new THREE.WebGLRenderer(); renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
+
     controls = new THREE.OrbitControls(camera);
     controls.addEventListener('change', render)
 
-    var geometry = new THREE.BoxGeometry(1,1,1);
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material ); scene.add( cube );
-    camera.position.z = 5;
+    camera.position.z = 50;
 
+    var req = new XMLHttpRequest();
+    req.onload = function () {
+        // HACK: polygons are given in global space, but we dont know the center. Hardcoded offset
+        var xoffset = 100;
+        var yoffset = 100;
+
+        var polygons = JSON.parse(this.responseText);
+        obj = new THREE.Object3D();
+        for (var i=0; i<polygons.length; i++) {
+            var item = polygons[i]['inset0'][0];
+            console.log(item);
+            var mesh = meshFromPolygon(item, xoffset, yoffset);
+            obj.add(mesh);
+        }
+
+        scene.add(obj);
+    }
+    req.open("get", "polygons.json", true);
+    req.send();
+
+    animate();
     render();
 }
 init();
+
